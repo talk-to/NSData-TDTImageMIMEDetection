637adc2bd84ba9561e1c5a6d4700b339ce35bf44
Ayush Goel <ayush.g@directi.com>
2014-05-09 17:37:47 +0530

    Move implementation of isJPEG to another func

--- NSData+ImageMIMEDetection/NSData+ImageMIMEDetection.m
+++ NSData+ImageMIMEDetection/NSData+ImageMIMEDetection.m
@@ -5,21 +5,24 @@ static NSUInteger const JPEGHeaderLength = 4;
 
 @implementation NSData (ImageMIMEDetection)
 
-- (NSString *)tdt_MIMEType {
+- (BOOL)isJPEG {
   if (self.length < JPEGHeaderLength) {
-    return nil;
+    return NO;
   }
+
   unsigned char buffer[JPEGHeaderLength];
   [self getBytes:&buffer length:JPEGHeaderLength];
 
   buffer[3] = buffer[3] & 0xF0;
 
-  BOOL isJPEG = (buffer[0] == 0xFF &&
-                 buffer[1] == 0xD8 &&
-                 buffer[2] == 0xFF &&
-                 buffer[3] == 0xE0);
+  return (buffer[0] == 0xFF &&
+          buffer[1] == 0xD8 &&
+          buffer[2] == 0xFF &&
+          buffer[3] == 0xE0);
+}
 
-  if (isJPEG) {
+- (NSString *)tdt_MIMEType {
+  if ([self isJPEG]) {
     return @"image/jpeg";
   }
 
