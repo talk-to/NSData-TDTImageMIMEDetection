9e96698b27f3b73351976cad433cbdd79bbe9f4d
Ayush Goel <ayush.g@directi.com>
2014-05-09 17:26:54 +0530

    Add check for JPEG image

--- NSData+ImageMIMEDetection/NSData+ImageMIMEDetection.m
+++ NSData+ImageMIMEDetection/NSData+ImageMIMEDetection.m
@@ -1,6 +1,27 @@
 
 #import "NSData+ImageMIMEDetection.h"
 
+static NSUInteger const JPEGHeaderLength = 4;
+
 @implementation NSData (ImageMIMEDetection)
+
+- (NSString *)tdt_MIMEType {
+  if (self.length < JPEGHeaderLength) {
+    return nil;
+  }
+  unsigned char buffer[JPEGHeaderLength];
+  [self getBytes:&buffer length:JPEGHeaderLength];
+
+  buffer[3] = buffer[3] & 0xF0;
+
+  BOOL isJPEG = (buffer[0] == 0xFF &&
+                 buffer[1] == 0xD8 &&
+                 buffer[2] == 0xFF &&
+                 buffer[3] == 0xE0);
+
+  if (isJPEG) {
+    return @"image/jpeg";
+  }
+}
 
 @end
