3679a62c59dde625a18416f15aadaa6845bc8cfd
Ayush Goel <ayush.g@directi.com>
2014-05-09 16:58:14 +0530

    Add interface for the category

--- NSData+ImageMIMEDetection/NSData+ImageMIMEDetection.h
+++ NSData+ImageMIMEDetection/NSData+ImageMIMEDetection.h
@@ -1,4 +1,14 @@
 
 @interface NSData (ImageMIMEDetection)
 
+/**
+ Try to deduce the MIME type.
+
+ @return string representation of detected MIME type. `nil` if not able to
+ detect the MIME type.
+
+ @see http://en.wikipedia.org/wiki/Internet_media_type
+ */
+- (NSString *)tdt_MIMEType;
+
 @end
