06e9fbcae9e609b8e851ee8f4ed4944240a81ba3
Ayush Goel <ayush.g@directi.com>
2014-05-09 17:38:12 +0530

    Add PNG check function

--- NSData+ImageMIMEDetection/NSData+ImageMIMEDetection.m
+++ NSData+ImageMIMEDetection/NSData+ImageMIMEDetection.m
@@ -2,6 +2,7 @@
 #import "NSData+ImageMIMEDetection.h"
 
 static NSUInteger const JPEGHeaderLength = 4;
+static NSUInteger const PNGHeaderLength = 4;
 
 @implementation NSData (ImageMIMEDetection)
 
@@ -21,9 +22,28 @@ static NSUInteger const JPEGHeaderLength = 4;
           buffer[3] == 0xE0);
 }
 
+- (BOOL)isPNG {
+  if (self.length < PNGHeaderLength) {
+    return NO;
+  }
+
+  unsigned char buffer[PNGHeaderLength];
+  [self getBytes:&buffer length:PNGHeaderLength];
+
+  return (buffer[0] == 0x89 &&
+          buffer[1] == 0x50 &&
+          buffer[2] == 0x4E &&
+          buffer[3] == 0x47);
+
+}
+
 - (NSString *)tdt_MIMEType {
   if ([self isJPEG]) {
     return @"image/jpeg";
+  }
+
+  if ([self isPNG]) {
+    return @"image/png";
   }
 
   return nil;
